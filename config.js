4f61992050c048a76f8360f04d15063064e9c102
mikeyouga <61699763+mikeyouga@users.noreply.github.com>
2020-03-10 12:40:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -53,9 +53,9 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "hallo？",
-        play: "音乐！",
-        bannar_coming: "上色！",
+        turn_on: "hallo",
+        play: "音乐",
+        bannar_coming: "上色",
         balloons_flying: "好像少点东西？",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
