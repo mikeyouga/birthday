4d81e37c396b2dfaadfcc79f8ac0ee99bf41d32a
mikeyouga <61699763+mikeyouga@users.noreply.github.com>
2020-03-10 11:28:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -51,6 +51,7 @@ var config = {
      */
     imgs: {
         "心爱的小可爱": "./imgs/huhu.jpg",
+        "我爱你":"./imgs/zhuzhu.jpg"
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
